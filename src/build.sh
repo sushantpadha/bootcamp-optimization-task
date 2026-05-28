b1c5c2b9b2ce76326ad7ac48905dc2b0620e926e
Akshat Shukla <ashukla.24@kgpian.iitkgp.ac.in>
2026-05-28 19:16:16 +0000

    Update Neon version with new optimizations

--- src/build.sh
+++ src/build.sh
@@ -11,7 +11,7 @@ CXX="${CXX:-g++-14}"
 # CXXFLAGS="-std=c++23 -O3 -mcpu=neoverse-v2 -Wall -Wextra"
 CXXFLAGS="-std=c++23 -O3 -mcpu=native -Wall -Wextra"
 OUTPUT="${OUTPUT:-spawn_sim}"
-INPUT="${INPUT:-src/spawn_sim_neon}"
+INPUT="${INPUT:-src/spawn_sim_neon_ptrstep_eor3_dualpartial_rowstep_inplace.cpp}"
 
 echo "Building with $CXX from $INPUT ..."
 "$CXX" $CXXFLAGS "$INPUT" -o "$OUTPUT" -lpthread
